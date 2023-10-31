d834cb1a1612a4f6b77508c8bc750adf7270e84c
MainaJoseph <mainajm254@gmail.com>
2023-10-31 18:01:28 +0300

    Added undo and redo in the color picker

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { SketchPicker } from "react-color";
 import { useSnapshot } from "valtio";
+import { FaRedoAlt, FaUndo } from "react-icons/fa";
 
 import state from "../store";
 
 const ColorPicker = () => {
   const snap = useSnapshot(state);
   const [displayColorCode, setDisplayColorCode] = useState(false);
+  const colorHistory = useRef([snap.color]);
+  const historyIndex = useRef(0);
 
   const buttonStyle = {
     backgroundColor: snap.color,
@@ -17,12 +20,37 @@ const ColorPicker = () => {
     borderColor: snap.color,
   };
 
+  const handleColorChange = (color) => {
+    const newColor = color.hex;
+    if (newColor !== snap.color) {
+      // Add the new color to the history
+      colorHistory.current.splice(historyIndex.current + 1);
+      colorHistory.current.push(newColor);
+      historyIndex.current = colorHistory.current.length - 1;
+      state.color = newColor;
+    }
+  };
+
+  const undo = () => {
+    if (historyIndex.current > 0) {
+      historyIndex.current--;
+      state.color = colorHistory.current[historyIndex.current];
+    }
+  };
+
+  const redo = () => {
+    if (historyIndex.current < colorHistory.current.length - 1) {
+      historyIndex.current++;
+      state.color = colorHistory.current[historyIndex.current];
+    }
+  };
+
   return (
     <div className="absolute left-full ml-3">
       <SketchPicker
         color={snap.color}
         disableAlpha
-        onChange={(color) => (state.color = color.hex)}
+        onChange={handleColorChange}
       />
       <div className="mt-2">
         <button
@@ -37,12 +65,26 @@ const ColorPicker = () => {
             <input
               type="text"
               value={snap.color}
-              onChange={(e) => (state.color = e.target.value)}
-              className="border rounded-md p-2"
+              onChange={(e) => handleColorChange({ hex: e.target.value })}
+              className="border rounded-md p-2 text-center font-semibold"
               style={inputStyle}
             />
           </div>
         )}
+        <div className="mt-2 flex items-center">
+          <button
+            onClick={undo}
+            className="text-white bg-gray-400 hover:bg-gray-500 py-2 px-3 rounded mr-2"
+          >
+            <FaUndo />
+          </button>
+          <button
+            onClick={redo}
+            className="text-white bg-gray-400 hover-bg-gray-500 py-2 px-3 rounded"
+          >
+            <FaRedoAlt />
+          </button>
+        </div>
       </div>
     </div>
   );
